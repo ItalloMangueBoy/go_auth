89b1bb43663bacfa887e139d56bfa7c897f3d238
mkspop <itallomkspop@gmail.com>
2026-01-13 22:59:36 -0300

    user table

--- src/models/user.go
+++ src/models/user.go
@@ -7,26 +7,27 @@ import (
 
 type User struct {
 	ID        uint64    `json:"id"`
-	Name      string    `json:"name"       gorm:"not null"               validate:"required"`
-	Email     string    `json:"email"       gorm:"not null"               validate:"required"`
-	Password  string    `json:"password"       gorm:"not null"               validate:"required"`
+	Name      string    `json:"name"       gorm:"not null"  validate:"required"`
+	Email     string    `json:"email"      gorm:"not null"  validate:"required"`
+	Password  string    `json:"password"   gorm:"not null"  validate:"required"`
 	CreatedAt time.Time `json:"created_at" gorm:"not null"`
 	UpdatedAt time.Time `json:"updated_at" gorm:"not null"`
 }
 
 // Format formats User fields for better presentation
 func (a *User) Format() *User {
-	// a.Name = helpers.FormatString(a.Name)
-	// a.Biography = helpers.FormatOptionalString(a.Biography)
+	a.Name = helpers.FormatString(a.Name)
+	a.Email = helpers.FormatString(a.Email)
+	a.Password = helpers.FormatString(a.Password)
 	return a
 }
 
 // Valid returns custom validation error messages for User
 func (a *User) Valid() helpers.ErrorMessages {
 	customMessages := helpers.ErrorMessages{
-		// "User.Name.required":      "Name is required",
-		// "User.Age.required":       "Age is required",
-		// "User.Biography.required": "Biography is required",
+		"User.Name.required":     "Name is required",
+		"User.Email.required":    "Email is required",
+		"User.Password.required": "Password is required",
 	}
 	return helpers.ValidateStruct(a, customMessages)
 }
